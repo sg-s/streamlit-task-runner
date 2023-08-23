d3d570a96bebcedd159fdccbae215e7253581f3e
Srinivas Gorur-Shandilya <code@srinivas.gs>
2023-08-23 15:32:03 -0400

    nicer explanation

--- task_runner.py
+++ task_runner.py
@@ -2,9 +2,25 @@ import streamlit as st
 from streamlit_task_runner import run_task, show_results
 
 st.write("# Task runner")
+st.write(
+    """This page demonstrates how we can run a long
+    running task in streamlit, capture output and live
+    and display it to the user.
 
+This is designed to work with any shell command,
+and here we use ping to simulate a long-running task.
 
-cmd = "ping -c 3 google.com"
+    """
+)
+
+n_ping = st.number_input(
+    "Number of ping packets",
+    min_value=3,
+    max_value=20,
+    step=1,
+)
+
+cmd = f"ping -c {n_ping} google.com"
 cmd = cmd.split()
 
 
@@ -12,7 +28,7 @@ empty_container = st.empty()
 
 
 do_stuff = st.button(
-    "Do stuff",
+    "Run",
     on_click=run_task,
     kwargs={"command": cmd, "container": empty_container},
 )
